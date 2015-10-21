57158f06d6b4c87a6154487e7a60a12a9387e596
Jenna Maloney <jmalo@uw.edu>
2015-10-21 12:45:59 -0700

    add images

--- app/app.php
+++ app/app.php
@@ -6,10 +6,15 @@
     //INITIALIZE COOKIE SESSION
     session_start();
 
+    $da_words = array('candy', 'yodle', 'dinosaur', 'paper', 'bottle', 'yep', 'drinking', 'tablet', 'monitor', 'candle', 'nope');
+    $da_letters = str_split(array_rand(array_flip($da_words)));
+
     if(empty($_SESSION['play_on']))
     {
-        $_SESSION['play_on'] = array();
+        $_SESSION['play_on']['yays'] = $da_letters;
+        $_SESSION['play_on']['nays'] = array();
     }
+
 
     //INITIALIZE APPLICATION (n uhh, find out what is the up w this "debug" thang)
     $app = new Silex\Application();
@@ -17,37 +22,43 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__."/../views"));
 
     //ROUTES
-    // $app->get('/slimeman', function () use ($app)
-    // {
-    //     return $app['twig']->render('slimeman.html.twig');
-    // });
+    $app->get('/', function () use ($app)
+    {
+        $da_letters = Guess::getDaLetters();
+
+        $letters_matching_guess = array();
+
+        foreach ($da_letters as $possible_match)
+        {
+            if ($possible_match->matchMaker($_GET['letter'], $possible_match))
+            {
+                array_push($letters_matching_guess, $possible_match);
+            }
+        }
+
+        return $app['twig']->render('slimeman.html.twig', array('letters_matching_guess' => $letters_matching_guess));
+    });
 
     $app->get('/new_guess', function () use ($app)
     {
         return $app['twig']->render('new_guess.html.twig');
     });
 
-    $app->post('/slimeman', function () use ($app)
+    $app->post('/', function () use ($app)
     {
-        $da_guess = new Guess($_GET['letter']);
-        $da_guess->save();
+        $attempts = count($_SESSION['play_on']['nays'];
+        //(i can't use getAttempts method here? if so, how?)
 
-        return $app['twig']->render('slimeman.html.twig', array('new_guess' => $da_guess));
-    });
+        $da_letters = ($_SESSION['play_on']['yays']);
 
-    $app->get('/slimeman', function () use ($app)
-    {
-        $already = Guess::getAll();
+        $new_guess = new Guess($_GET['letter'], $attempts, $da_letters);
 
-        $chosen_one = Guess::chosenWord();
-
-        $correct_guess = array();
-
-        foreach ($already as $or_nah)
-        {
-            if ($or_nah->nayorYay($_GET['']))
+        foreach
+        
         }
-    })
+
+        return $app['twig']->render('slimeman.html.twig');
+    });
 
     // $app->post('/slimeman', function () use ($app)
     // {
@@ -58,6 +69,7 @@
     //     $da_guess = new Guess($_GET['letter']);
     //
     //     //search da_letters array for da_guess
+
     //
     //     return $app['twig']->render('slimeman.html.twig')
     //
--- src/Guess.php
+++ src/Guess.php
@@ -1,52 +1,122 @@
 <?php
 class Guess
 {
-    //properties
+    //PROPERTIES
     private $letter;
+    //most current letter guessed
+    private $attempts;
+    //how many wrong guesses preceded this instance
+    private $da_letters;
+    //letters being compared to, fro the chosen word
+    private $message;
 
-    function __construct($a_letter)
+    function __construct($letter, $attempts, $da_letters, $message)
     {
-        $this->letter = $a_letter;
+        $this->letter = $letter;
+        $this->attempts = $attempts;
+        $this->da_letters = $da_letters;
+        $this->message = $message;
     }
 
-    //methods
-    function setLetter($new_letter)
-    {
-        $this->letter = $new_letter;
-    }
-
+    //METHODS
     function getLetter()
     {
         return $this->letter;
     }
 
-    function guessesRemaining()
+    function setLetter($new_letter)
     {
-        count($_SESSION['play_on']['nays']);
-        //use this for how many spaces?.. or foreach loops displaying "_" otherwise. count($_SESSION['play_on']['choice']);
-                //how many letters are currently in the array of guesses. count em
+        $this->letter = $new_letter;
     }
 
-    function chosenWord()
+    function getAttempts()
     {
-        $da_words = array('candy', 'yodle', 'dinosaur', 'paper', 'bottle', 'yep', 'drinking', 'tablet', 'monitor', 'candle', 'nope');
-        $da_letters = str_split(array_rand(array_flip($da_words)));
+        $attempts = (count($_SESSION['play_on']['nays']));
+        if ($attempts == 0)
+        {
+            $attempts = 'images/slimeman0.jpg';
+        }
+        elseif ($attempts == 1)
+        {
+            $attempts = 'images/slimeman1.jpg';
+        }
+        elseif ($attempts == 2)
+        {
+            $attempts = "images/slimeman2.jpg";
+        }
+        elseif ($attempts == 3)
+        {
+            $attempts = "images/slimeman3.jpg";
+        }
+        elseif ($attempts == 4)
+        {
+            $attempts = "images/slimeman4.jpg";
+        }
+        elseif ($attempts == 5)
+        {
+            $attempts = "images/slimeman5.jpg";
+        }
+        else
+        {
+            $attempts = "images/slimeman7gameover.jpg";
+        }
+
+        return $this->attempts;
+    }
+
+    function setAttempts($new_attempts)
+    {
+        $this->attempts = $new_attempts;
+    }
+
+    function getDaLetters()
+    {
+        $da_letters = ($_SESSION['play_on']['yays']);
 
         return $da_letters;
     }
 
-    function nayorYay($yesletters, $da_letters)
+    function setDaLetters($new_da_letters)
     {
+        $this->da_letters = $new_da_letters;
+    }
 
+    function getMessage()
+    {
+        return ""
+    }
+
+    function setMessage($new_message)
+    {
+        $this->message = $new_message;
+    }
+    // function nayorYay($yesletters, $da_letters)
+    // {
+    //     $da_letters =
+        //i wanna compare the letter, to the array of letters composing chosen word
         //method to match up the arrays from the guess word and chosen word
         //does the letter match any of the letters in the array that the string was split into?
         //is it already in the bank of words guessed thus far? if it matches any of those, return a string telling them to try agian
+    // }
+
+    function matchMaker($letter, $un_letter)
+    {
+        return ($letter == $un_letter);
     }
 
-    function saveYay()
+    function save()
     {
-        array_push($_SESSION['play_on']['yays'], $this);
+        array_push($_SESSION['play_on'], $this);
     }
+    // static function saveYay()
+    // {
+    //     array_push($_SESSION['play_on']['yays'], $this);
+    // }
+    //
+    // static function saveNay()
+    // {
+    //     array_push($_SESSION['play_on']['nays'], $this);
+    // }
 
     static function getAll()
     {
@@ -56,7 +126,7 @@ class Guess
     //implement this function after the 7th guess, using  a button labeled play again, linking to new guess page
     static function deleteAll()
     {
-        $_SESSION['[play_on'] = array();
+        $_SESSION['play_on'] = array();
     }
 
 }

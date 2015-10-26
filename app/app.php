2851249f9a3b8f565b0f8d2eea17d18fb841c484
Jenna Maloney <jmalo@uw.edu>
2015-10-26 16:29:13 -0700

    add delete button which performs the correct function, but is returning an error message stating slimeman.html.twig is not defined

--- app/app.php
+++ app/app.php
@@ -7,22 +7,27 @@
     session_start();
 
     $da_words = array('candy', 'yodle', 'dinosaur', 'paper', 'bottle', 'yep', 'drinking', 'tablet', 'monitor', 'candle', 'nope');
-    $first_one = array('0');
     $winning_letters = str_split(array_rand(array_flip($da_words)));
-    $winning_letters = array_merge($first_one, $winning_letters);
+    // $first_one = array('0');
+    // $winning_letters = str_split(array_rand(array_flip($da_words)));
+    // $winning_letters = array_merge($first_one, $winning_letters);
     $winning_instances = array();
-    foreach ($winning_letters as $w)
-    {
-        array_push($winning_instances, ($w = new Guess(current($winning_letters))), (next($winning_letters)));
-    }
     //
-    // if(empty($_SESSION['play_on']))
+    // foreach ($winning_letters as $w)
     // {
-        $_SESSION['play_on']['wins'] = $winning_instances;
-        $_SESSION['play_on']['yays'] = array('k', 'c', 'd', 'e', 'o', 'r', 'a', 'b', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'p', 's', 't', 'u', 'w', 'y');
-        $_SESSION['play_on']['nays'] = array();
+    //
     // }
-
+    // foreach ($winning_letters as $w)
+    // {
+    //     array_push($winning_instances, ($w = new Guess(current($winning_letters))), (next($winning_letters)));
+    // }
+    //
+    if(empty($_SESSION['play_on']['wins']))
+    {
+        $_SESSION['play_on']['wins'] = $winning_letters;
+        $_SESSION['play_on']['yays'] = array();
+        $_SESSION['play_on']['nays'] = array();
+    }
 
     //INITIALIZE APPLICATION (n uhh, find out what is the up w this "debug" thang)
     $app = new Silex\Application();
@@ -50,6 +55,9 @@
                  array_push($matched_letters, '__');
              }
          }
+         var_dump($guessed_letters);
+         var_dump($matched_letters);
+         var_dump($golden_letters);
 
         return $app['twig']->render('slimeman.html.twig', array('matched_letters' => $matched_letters));
     });
@@ -59,12 +67,12 @@
         return $app['twig']->render('new_guess.html.twig');
     });
 
-    $app->post('/new_guess', function () use ($app)
+    $app->post('/', function () use ($app)
     {
         $new_letter = new Guess($_POST['letter']);
-        $prize_letters = Guess::getWins();
+        $golden_letters = Guess::getWins();
 
-            if (in_array($new_letter, $prize_letters))
+            if (in_array($new_letter, $golden_letters))
             {
                 $new_letter->saveYay();
             }
@@ -72,9 +80,37 @@
             {
                 $new_letter->saveNay();
             }
-            var_dump($prize_letters);
 
-        return $app['twig']->render('slimeman.html.twig', array('new_letter' => $new_letter));
+        $guessed_letters = Guess::getYays();
+        $incorrect_guesses = Guess::getNays();
+
+        $matched_letters = array();
+
+        foreach ($golden_letters as $g)
+        {
+            if(in_array($g, $guessed_letters))
+            {
+                array_push($matched_letters, $g);
+             }
+             else
+             {
+                 array_push($matched_letters, '__');
+             }
+         }
+            var_dump($new_letter);
+            var_dump($golden_letters);
+            var_dump($guessed_letters);
+            var_dump($incorrect_guesses);
+            var_dump($matched_letters);
+
+        return $app['twig']->render('slimeman.html.twig', array('matched_letters' => $matched_letters));
+    });
+
+    $app->post('/restart_game', function () use ($app)
+    {
+        Guess::deleteAll();
+
+        return $app['twig']->render('slimeman.twig.html');
     });
 
     return $app;
